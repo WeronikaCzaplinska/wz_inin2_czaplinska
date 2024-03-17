02f439fb92337aa55f71e35056882c97f5bc8443
Weronika Czaplinska <gax36193@student.gdynia.merito.pl>
2024-03-17 14:08:21 +0100

    Obiektowość

---
+++ src/Film.java
@@ -0,0 +1,14 @@
+public class Film extends Autor {
+    String name_movie;
+    int year_movie;
+
+    public void info () {
+        System.out.println("Nazwa filmu, to " + name_movie
+                + ". Rok produkcji filmu, to " + year_movie
+                + ". Autor filmu, to " + this.getName_author()
+                + ", a jego rok urodzenia, to " + this.getYear_author()
+
+        );
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String [] args) {
+
+        Film movie = new Film();
+        movie.name_movie = "Avatar";
+        movie.year_movie = 2018;
+        movie.setName_author("Andrzej");
+        movie.setYear_author(2000);
+        movie.info();
+
+        Car car = new Car("BMW", 2024, "95", "czerwony");
+        System.out.println(car.infoCar());
+
+    }
+}
+
