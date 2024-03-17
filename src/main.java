fa3395a68e56fc5701fb2da5e75666bf0e74b557
Weronika Czaplinska <gax36193@student.gdynia.merito.pl>
2024-03-17 12:23:54 +0100

    Projekt Java

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String [] args) {
+        System.out.println("Hello World!");
+
+        for (int i = 1; 1 <= 5; i++) {
+            System.out.println("i = " + 1);
+        }
+    }
+}
